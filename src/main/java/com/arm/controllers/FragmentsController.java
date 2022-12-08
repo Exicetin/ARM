e6b514166ef96d07f814b6c286da62ede4d6ab35
Ксения Злакоманова <zlakomanovakv@gmail.com>
2022-12-08 03:07:53 +0300

    Новые классы

---
+++ src/main/java/com/arm/controllers/FragmentsController.java
@@ -0,0 +1,14 @@
+package com.arm.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FragmentsController {
+
+    @GetMapping("/menu")
+    public String getMenu() {
+        return "menu";
+    }
+}
